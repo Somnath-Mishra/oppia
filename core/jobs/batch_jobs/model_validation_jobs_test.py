d6927d5106e948ff99c3efd2665b378446c1f7f9
Somnath <somnath012003@gmail.com>
2025-02-14 18:29:35 +0530

    fix some minor nit in code style

--- core/jobs/batch_jobs/model_validation_jobs_test.py
+++ core/jobs/batch_jobs/model_validation_jobs_test.py
@@ -34,10 +34,9 @@ if MYPY: # pragma: no cover
     from mypy_imports import base_models
     from mypy_imports import user_models
 
-(auth_models, base_models, user_models) = (
-    models.Registry.import_models([
+(auth_models, base_models, user_models) = models.Registry.import_models([
     models.Names.AUTH, models.Names.BASE_MODEL, models.Names.USER
-]))
+])
 datastore_services = models.Registry.import_datastore_services()
 
 
